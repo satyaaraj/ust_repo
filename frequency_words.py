a5ca7e8b41b7f8e0c940515c05d8ff4349e21242
satyaaraj <34859661+satyaaraj@users.noreply.github.com>
2024-11-30 13:44:47 +0530

    Add files via upload
    this file has sorting the list using sorted method

--- frequency_words.py
+++ frequency_words.py
@@ -1,6 +1,5 @@
 string = "which is better python 2 or python 3"
 str_split = string.split()
-str_split.sort()
 d = {}
 for st in str_split:
     i = 1
@@ -8,7 +7,7 @@ for st in str_split:
         d[st] = i
     else:
         d[st] = 1
-print(d)
 list_d = list(d.items())
-for ls in list_d:
-    print(ls)
+sorted_list = sorted(list_d)
+for i in sorted_list:
+    print(i)
